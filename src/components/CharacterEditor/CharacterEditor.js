6ab92cb92590b3211ef418d193343dafe964f09b
Travis Ricks <ricks.travis@gmail.com>
2021-12-12 18:51:04 +0900

    Exercise 4 - add background color for perspective effect

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -1,12 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import {
-  defaultSkinColor,
-  defaultClothesColor,
-} from '../../constants';
-import Character from '../Character';
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import ControlPane from '../ControlPane';
+import { defaultSkinColor, defaultClothesColor } from "../../constants";
+import Character from "../Character";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import ControlPane from "../ControlPane";
 
 import {
   bodyOptions,
@@ -14,9 +11,9 @@ import {
   faceOptions,
   accessoryOptions,
   skinColorOptions,
-  clothesColorOptions,
-} from './CharacterEditor.helpers';
-import styles from './CharacterEditor.module.css';
+  clothesColorOptions
+} from "./CharacterEditor.helpers";
+import styles from "./CharacterEditor.module.css";
 
 function App() {
   const [body, setBody] = React.useState(0);
@@ -24,18 +21,17 @@ function App() {
   const [face, setFace] = React.useState(0);
   const [accessory, setAccessory] = React.useState(0);
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor);
-  const [clothesColor, setClothesColor] = React.useState(
-    defaultClothesColor
-  );
+  const [clothesColor, setClothesColor] = React.useState(defaultClothesColor);
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveEffect}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
-            Customize your character's look and style using the
-            controls below. What sort of adventure will you embark on?{' '}
+            Customize your character's look and style using the controls below.
+            What sort of adventure will you embark on?{" "}
           </p>
         </header>
         <div className={styles.controlColumn}>
